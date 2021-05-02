388aeb6ec554374203fc08acb3f83f13c1f1326e
josema1823 <josemaflorez1823@gmail.com>
2021-05-02 10:07:41 -0500

    configuration gh-pages

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<douzedeveloper>.github.io/<REPO>
+git push -f git@github.com:<douzedeveloper>/<newshore>.git master:gh-pages
+
+cd -
